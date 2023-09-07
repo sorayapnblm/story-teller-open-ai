bed38f579d7965d94a02904a7871e887db961777
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-07 22:32:59 +0400

    Delete comments and add new one Add a system message to determine how we want the AI to act

--- server/index.js
+++ server/index.js
@@ -1,55 +1,45 @@
 const OpenAI = require("openai");
-// import { Configuration, OpenAIApi } from "openai";
-const express = require('express');
-const bodyParser = require('body-parser')
-const cors = require('cors')
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
-const port = 3080
+const port = 3080;
 
 const openai = new OpenAI({
-    organization: "org-tI54CUkXDICLxrt34bIoBqvN",
-    apiKey: process.env.OPENAI_API_KEY
+  organization: "org-tI54CUkXDICLxrt34bIoBqvN",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const app = express();
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
-// async function callApi() {
-//     const completion = await openai.chat.completions.create(
-//         {
-//         messages: [
-//             { role: "system", 
-//             content: "You are a helpful assistant." 
-//             }
-//         ],
-//         model: "gpt-3.5-turbo",
-//         }
-//     );
-//     console.log(completion.choices[0].message.content);
-// }
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  console.log("const { message } = req.body:    ", message);
 
-// Create a simple express api that calls the function above
+  const completion = await openai.chat.completions.create({
+    messages: [
+        { "role": "system", "content": "Talk like a fairy."},
+        { "role": "user", "content": `${message}`}
+    ],
+    model: "gpt-3.5-turbo",
+  });
 
-app.post('/', async (req, res) => {
-    const { message } = req.body;
-    console.log(message, "message")
-    const completion = await openai.chat.completions.create(
-        {
-        messages: 
-        [
-            { role: "system", 
-            content: `${message}` 
-            }
-        ],
-        model: "gpt-3.5-turbo",
-        }
-    );
-    res.json({
-        message: completion.choices[0].message.content,
-    })
-})
+    // const completion = await openai.chat.completions.create({
+    //   messages: [{ "role": "system", "content": `${message}` }],
+    //   model: "gpt-3.5-turbo",
+    // });
+
+  console.log(completion.choices[0].message);
+  res.json({
+    message: completion.choices[0].message.content,
+  });
+
+});
+
+
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
