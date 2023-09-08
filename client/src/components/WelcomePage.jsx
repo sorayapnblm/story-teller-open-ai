4d1223e51fca3ac5a74cfcd97f832959896fa962
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-08 18:54:28 +0400

    Create the WelcomePage component for te welcome page for the UI

---
+++ client/src/components/WelcomePage.jsx
@@ -0,0 +1,15 @@
+// WelcomePage.jsx
+
+import React from "react";
+import "../styles/WelcomePage.css"
+
+function WelcomePage() {
+  return (
+    <div className="welcome-page">
+      <h1>Welcome to ChatApp</h1>
+      <p>Start a new chat session or choose an existing one to begin chatting.</p>
+    </div>
+  );
+}
+
+export default WelcomePage;
