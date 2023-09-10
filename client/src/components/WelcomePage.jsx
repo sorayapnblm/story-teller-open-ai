7962c27424858f67c78f24a15df6aaebedfe3861
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-10 14:13:57 +0200

    Add an image on the Welcome.jsx file

--- client/src/components/WelcomePage.jsx
+++ client/src/components/WelcomePage.jsx
@@ -1,13 +1,25 @@
-// WelcomePage.jsx
-
 import React from "react";
 import "../styles/WelcomePage.css"
+import  test from "../assets/Untitled_Artwork 2.png"
+import { Link } from "react-router-dom";
 
 function WelcomePage() {
   return (
     <div className="welcome-page">
-      <h1>Welcome to ChatApp</h1>
-      <p>Start a new chat session or choose an existing one to begin chatting.</p>
+      <div className="welcome-intro">
+        <h1>
+          Hi it's me Soraya <br />
+          Welcome on "Tell me a story"<br />
+          You will be able to create your own story with an artficial intelligence. <br />
+          In this game, you will be able to choose your story-tellerg
+        </h1>
+        <Link to="/chat" className="start-button">
+          Start Chat
+        </Link>
+      </div>
+      <div className="welcome-image-container">
+      <img src={test} className="welcome-image"/>
+      </div>
     </div>
   );
 }
