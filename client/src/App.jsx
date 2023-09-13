da1f8f112e80d88d047d8b189dea3d0349f675a7
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-13 16:39:46 +0100

    Create a route for Signup.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage.jsx";
 import ChatApp from "./components/Chat.jsx";
+import Signup from "./components/Signup.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        {/* Define more routes here */}
         <Route path="/chat" element={<ChatApp />} />
+        <Route path="/register" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
