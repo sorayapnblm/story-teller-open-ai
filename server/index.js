07bc3f59736718f25a375c6ea2c84edd02b76d6c
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-13 22:50:56 +0100

    Add API for the "login" route

--- server/index.js
+++ server/index.js
@@ -54,4 +54,21 @@ mongoose.connection.on('open', () => {
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
+
+  app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    UserModel.findOne({email: email})
+    .then(user => {
+      if (user) {
+        if (user.password === password) {
+          res.json("Success")
+        } else {
+          res.json("The password is incorrect")
+        }
+      } else {
+        res.json("No record existed")
+      }
+    })
+  })
 });
+
