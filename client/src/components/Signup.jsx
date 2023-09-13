4c94811e307e95af63e234df98a9eeb635cdb034
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-13 16:18:28 +0100

    Create Signup component

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className='constainer1'>
+        <div className="container2">
+            <h2>Register</h2>
+            <form>
+                <div className='container3'>
+                    <label htmlFor="email">
+                        <strong>Name</strong>
+                    </label>
+                    <input 
+                    type="text"
+                    placeholder='Enter Name'
+                    autoComplete='off'
+                    name='email'
+                    className='container-form' 
+                    />
+                </div>
+                <div className='container3'>
+                    <label htmlFor="email">
+                        <strong>Email</strong>
+                    </label>
+                    <input 
+                    type="text"
+                    placeholder='Enter Email'
+                    autoComplete='off'
+                    name='email'
+                    className='container-form' 
+                    />
+                </div>
+                <div className='container3'>
+                    <label htmlFor="email">
+                        <strong>Password</strong>
+                    </label>
+                    <input 
+                    type="password"
+                    placeholder='Enter Password'
+                    autoComplete='off'
+                    name='password'
+                    className='container-form' 
+                    />
+                </div>
+                <button type='submit' className='button'>
+                    Register
+                </button>
+                <p>
+                    Already have an account ?
+                </p>
+                <button className='button1'>
+                    Login
+                </button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Signup
