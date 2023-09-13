94c469e6a601301ad10ff122e68ace71337054d3
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-13 21:36:13 +0100

    Connect client side to server side

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+
 
 const Login = () => {
-  return (
-    <div className='constainer1'>
-        hhh
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3080/login", {email, password })
+      .then((result) => {
+        console.log(result);
+        navigate("/chat");
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div className="constainer1">
+      <div className="container2">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="container3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              autoComplete="off"
+              name="email"
+              className="container-form"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="container3">
+            <label htmlFor="email">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              autoComplete="off"
+              name="password"
+              className="container-form"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="button">
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
