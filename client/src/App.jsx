b268b236fb69efc04100704cdac0a197cd58bc0d
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-14 20:50:06 +0100

    Create ChatInterface.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage.jsx";
-import ChatApp from "./components/Chat.jsx";
 import Signup from "./components/Signup.jsx";
 import Login from "./components/Login.jsx";
+import ChatInterface from "./components/ChatInterface.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/chat" element={<ChatApp />} />
+        <Route path="/chat" element={<ChatInterface />} />
         <Route path="/register" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
---
+++ client/src/components/ChatInterface.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ChatBox from './ChatBox'
+
+const ChatInterface = () => {
+  return (
+    <ChatBox/>
+  )
+}
+
+export default ChatInterface
