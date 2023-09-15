581ab508a262b45be7a42dd46d4156e5e22f12bb
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-15 14:30:26 +0100

    Create the component CustomizeStory.jsx to handle the choices made by the user to customize the story

---
+++ client/src/components/CustomizeStory.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CustomizeStory = () => {
+  return (
+    <div className='customize-story'>
+        <div className='customize-story-intro'>
+            Let's first customize your story !
+        </div>
+    </div>
+  )
+}
+
+export default CustomizeStory
