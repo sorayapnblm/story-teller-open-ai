a2110de087f2a452fb123d6f27620a407fe0e1e6
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-16 21:44:47 +0100

    Add a way that change the style of the button when the user click on it

--- client/src/components/CustomizeStory.jsx
+++ client/src/components/CustomizeStory.jsx
@@ -4,15 +4,14 @@ import "../styles/CustomizeStory.css";
 import { useParams } from "react-router-dom";
 
 const CustomizeStory = () => {
-
   // Get the character's name from the URL
-  const { storyteller, storytellername} = useParams();
+  const { storyteller, storytellername } = useParams();
 
   // Create state variables to store user selections
   const [mainCharacterName, setMainCharacterName] = useState("");
-  const [selectedPronouns, setSelectedPronouns] = useState("");
+  const [selectedPronouns, setSelectedPronouns] = useState(null);
   const [topic, setTopic] = useState("");
-  const [selectedLanguage, setSelectedLanguage] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState(null);
 
   // Handle changes in the main character name input field
   const handleMainCharacterNameChange = (e) => {
@@ -41,42 +40,53 @@ const CustomizeStory = () => {
       </div>
       <div className="customize-story-inputs">
         <div className="customize-story-inputs-maincharacter">
-          <h2>
-          Choose your main character name:
-          </h2>
+          <h2>Choose your main character name:</h2>
           <input
             type="text"
             value={mainCharacterName}
             onChange={handleMainCharacterNameChange}
             placeholder="Enter main character name"
-          />
+            />
         </div>
 
         <div className="customize-story-inputs-pronouns">
-          <h2>
-          Choose your main character gender:
-          </h2>
+          <h2>Choose your main character gender:</h2>
           <ul>
             <li>
-              <button onClick={() => handlePronounsChange("She her hers")}>
-              She her hers
+              <button
+                onClick={() => handlePronounsChange("She her hers")}
+                className={
+                  selectedPronouns === "She her hers" ? "selected-button" : ""
+                }
+              >
+                She her hers
               </button>
             </li>
             <li>
-              <button onClick={() => handlePronounsChange("He him his")}>He him his</button>
+            <button
+                onClick={() => handlePronounsChange("He him his")}
+                className={
+                  selectedPronouns === "He him his" ? "selected-button" : ""
+                }
+              >
+                He him his
+              </button>
             </li>
             <li>
-              <button onClick={() => handlePronounsChange("They them their")}>
-              They them their
+            <button
+                onClick={() => handlePronounsChange("They them their")}
+                className={
+                  selectedPronouns === "They them their" ? "selected-button" : ""
+                }
+              >
+                They them their
               </button>
             </li>
           </ul>
         </div>
 
         <div className="customize-story-inputs-topic">
-          <h2>
-          Choose your topic:
-          </h2>
+          <h2>Choose your topic:</h2>
           <input
             type="text"
             value={topic}
@@ -86,46 +96,44 @@ const CustomizeStory = () => {
         </div>
 
         <div className="customize-story-inputs-language">
-          <h2>
-          Choose your language:
-          </h2>
+          <h2>Choose your language:</h2>
           <ul>
-            {[
-              "English",
-              "French",
-              "Spanish",
-              "Korean",
-              "Hindi"
-            ].map((language) => (
-              <li key={language}>
-                <button onClick={() => handleLanguageChange(language)}>
-                  {language}
-                </button>
-              </li>
-            ))}
+            {["English", "French", "Spanish", "Korean", "Hindi"].map(
+              (language) => (
+                <li key={language}>
+                  <button onClick={() => handleLanguageChange(language)}
+                className={
+                  selectedLanguage === language ? "selected-button" : ""
+                }>
+                    {language}
+                  </button>
+                </li>
+              )
+            )}
           </ul>
           <ul>
-            {[
-              "Arabic",
-              "Russian",
-              "Portuguese",
-              "Indonesian",
-              "Chinese"
-            ].map((language) => (
-              <li key={language}>
-                <button onClick={() => handleLanguageChange(language)}>
-                  {language}
-                </button>
-              </li>
-            ))}
+            {["Arabic", "Russian", "Portuguese", "Indonesian", "Chinese"].map(
+              (language) => (
+                <li key={language}>
+                  <button onClick={() => handleLanguageChange(language)}
+                className={
+                  selectedLanguage === language ? "selected-button" : ""
+                }>
+                    {language}
+                  </button>
+                </li>
+              )
+            )}
           </ul>
         </div>
 
         <div className="customize-story-link-container">
-        <Link to={`/chat/${storyteller}/${storytellername}/${mainCharacterName}/${selectedPronouns}/${topic}/${selectedLanguage}`} 
-        className="customize-story-link">
-          Start
-        </Link>
+          <Link
+            to={`/chat/${storyteller}/${storytellername}/${mainCharacterName}/${selectedPronouns}/${topic}/${selectedLanguage}`}
+            className="customize-story-link"
+          >
+            Start
+          </Link>
         </div>
       </div>
     </div>
