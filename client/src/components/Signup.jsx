bcd169dc0678603c2daa418e3aaa10fdcf93bb72
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-17 17:25:32 +0100

    Change the clesses name and the Link's text

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,84 +1,77 @@
-import React, { useState } from 'react'
-import "../styles/Signup.css"
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from "react";
+import "../styles/Signup.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const navigate = useNavigate()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios.post("http://localhost:3080/register", {name, email, password})
-        .then(result => {console.log(result)
-            navigate('/login')
-        })
-        .catch(err => console.log(err))
-
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:3080/register", { name, email, password })
+      .then((result) => {
+        console.log(result);
+        navigate("/login");
+      })
+      .catch((err) => console.log(err));
+  };
   return (
-    <div className='constainer1'>
-        <div className="container2">
-            <h2>Register</h2> 
-            <form onSubmit={handleSubmit}>
-                <div className='container3'>
-                    <label htmlFor="email">
-                        <strong>Name</strong>
-                    </label>
-                    <input 
-                    type="text" 
-                    placeholder='Enter Name'
-                    autoComplete='off'
-                    name='email'
-                    className='container-form' 
-                    onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
-                <div className='container3'>
-                    <label htmlFor="email">
-                        <strong>Email</strong>
-                    </label>
-                    <input 
-                    type="text"
-                    placeholder='Enter Email'
-                    autoComplete='off'
-                    name='email'
-                    className='container-form'
-                    onChange={(e) => setEmail (e.target.value)}
-
-                    />
-                </div>
-                <div className='container3'>
-                    <label htmlFor="email">
-                        <strong>Password</strong>
-                    </label>
-                    <input 
-                    type="password"
-                    placeholder='Enter Password'
-                    autoComplete='off'
-                    name='password'
-                    className='container-form' 
-                    onChange={(e) => setPassword(e.target.value)}
-
-                    />
-                </div>
-                <button type='submit' className='button'>
-                    Register
-                </button>
-                <p>
-                    Already have an account ?
-                </p>
-                </form>
-                <Link to="/login" className='button1'>
-                    Login
-                </Link>
-            
+    <div className="signup">
+      <h1>Sign up.</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="signup-container">
+          <h2>Name</h2>
+          <input
+            type="text"
+            placeholder="Enter Name"
+            autoComplete="off"
+            name="email"
+            className="signup-form"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
+        <div className="signup-container">
+          <h2>Email</h2>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            autoComplete="off"
+            name="email"
+            className="signup-form"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="signup-container">
+          <h2>Password</h2>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            autoComplete="off"
+            name="password"
+            className="signup-form"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="signup-button-container">
+        <button type="submit" className="signup-button">
+          Register
+        </button>
+        </div>
+      </form>
+      <div className="signup-login-button-container">
+      <h2>If you are registered, please log in.</h2>
+      <Link to="/login" className="signup-login-button">
+        Login
+      </Link>
+      </div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
