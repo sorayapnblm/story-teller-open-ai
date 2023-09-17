39a96135499a5618103be5c150c3c81fa8552bcf
sorayapnblm <sorayapanambalom@gmail.com>
2023-09-17 17:26:06 +0100

    Change the classes' name

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+import "../styles/Login.css"
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -26,42 +28,43 @@ const Login = () => {
   };
 
   return (
-    <div className="constainer1">
-      <div className="container2">
-        <h2>Login</h2>
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
+    <div className="login">
+        <h1>Log in.</h1>
         <form onSubmit={handleSubmit}>
-          <div className="container3">
-            <label htmlFor="email">
-              <strong>Email</strong>
-            </label>
+          <div className="login-container">
+            <h2>
+              Email
+            </h2>
             <input
               type="text"
               placeholder="Enter Email"
               autoComplete="off"
               name="email"
-              className="container-form"
+              className="login-form"
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-          <div className="container3">
-            <label htmlFor="email">
-              <strong>Password</strong>
-            </label>
+          <div className="login-container">
+            <h2>
+              Password
+            </h2>
             <input
               type="password"
               placeholder="Enter Password"
               autoComplete="off"
               name="password"
-              className="container-form"
+              className="login-form"
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button type="submit" className="button">
+          <div className="login-button-container">
+          <button type="submit" className="login-button">
             Start
           </button>
+          </div>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+          
         </form>
-      </div>
     </div>
   );
 };
